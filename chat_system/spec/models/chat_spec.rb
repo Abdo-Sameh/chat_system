0c3c5d2d3289320a83e1aea92264859f96d9f1c3
Abdelrahman Sameh <asameh@inmobly.com>
2022-10-07 00:34:02 +0200

    add some test cases

--- chat_system/spec/models/chat_spec.rb
+++ chat_system/spec/models/chat_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @application = Application.create(name: "Netflix", token: SecureRandom.hex(16))
+    @chat = Chat.create(chat_number: 1)
+  end
+
+  it 'checks that a chat can be created' do
+    expect(@chat).to be_valid
+  end
+
+  it 'checks that a chat can be read' do
+    expect(Chat.find_by_chat_number(1)).to eq(@chat)
+  end
+
+  it 'checks that a chat can be destroyed' do
+    @chat.destroy
+    expect(Application.chats).to eq(0)
+  end
 end
